a96a0c8b42fc30d320344d8932b257cf830c3389
Marco Antonio Lasmar Almada <ra092208@catatau.lab.ic.unicamp.br>
2013-04-16 22:01:18 -0300

    wtf

--- client.c
+++ client.c
@@ -35,6 +35,7 @@ int main(int argc, char *argv[])
     struct addrinfo hints, *servinfo, *p;
     int rv;
     char s[INET6_ADDRSTRLEN];
+    int estoque;
 
     if (argc != 2) {
         fprintf(stderr,"usage: client hostname\n");
@@ -82,8 +83,6 @@ int main(int argc, char *argv[])
 	char ISBN[20];
 	char msg[MAXDATASIZE];
 	char pass[20];
-        char treco[MAXDATASIZE];
-	char it;
 	int bytes_sent, len, bytes_rcv;
 	
 	while(1){
@@ -104,14 +103,22 @@ int main(int argc, char *argv[])
 			scanf(" %s", pass);
 			if(strcmp(pass, "senhalivraria") != 0)
 			continue;
+			else{
+			  printf("Digite o ISBN\n");
+			  scanf(" %s", ISBN);
+			  printf("Digite o novo valor do estoque\n");
+			  scanf("%d", &estoque);
+			}
 		}
-		if(opt == 2 || opt == 3 || opt == 5 || opt == 6){
+		if(opt == 2 || opt == 3 || opt == 6){
 			printf("Digite o ISBN\n");
 			scanf(" %s", ISBN);
 		}
 		
 		//escreve requisicao
-		sprintf(msg, "%d %s", opt, ISBN);
+		if (opt == 5) sprintf(msg, "%d %s %d", opt, ISBN, estoque);
+		else sprintf(msg, "%d %s", opt, ISBN);  
+		
 		
 		//pega tempo inicial
 		struct timeval tempo_in, tempo_fim;
@@ -129,22 +136,19 @@ int main(int argc, char *argv[])
 		        perror("erro no recv");
 		        break;
 		    }
-		    sscanf(buf, "%d", &cont);
-		    printf("%s", buf+2);
-/*		    it = 'c';	
-		    while (it != '\0'){
-			sscanf(buf, "%c", it);
-			printf("%c", it);
-		    }
-		    printf("%s", msg);
-		    if(cont == 0) break;*/
+		    sscanf(buf, "%d", &cont);    		    
+		    printf("%s", buf);
+		    if(cont == 0) break;
+		    //		    printf("%d\n", cont);
+		    //       		    printf("%s", buf+2);
+		    //		    printf("\n%d", cont);
 		}
 		
 		//pega tempo final
 		gettimeofday(&tempo_fim, NULL);
 		double tempo1, tempo2;
-		tempo1 = tempo_in.tv_sec + tempo_in.tv_usec;
-		tempo2 = tempo_fim.tv_sec + tempo_fim.tv_usec;
+		tempo1 = tempo_in.tv_sec + 0.000001*tempo_in.tv_usec;
+		tempo2 = tempo_fim.tv_sec + 0.000001*tempo_fim.tv_usec;
 		printf("\nTempo total: %lf\n", tempo2-tempo1);
 	}
 
